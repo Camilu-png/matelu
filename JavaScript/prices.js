617f88eaee3885145e0b089714cbb25075efbbf3
Camila Arancibia <camilachillan99@gmail.com>
2021-07-17 20:52:12 -0400

    coupons

--- JavaScript/prices.js
+++ JavaScript/prices.js
@@ -1,3 +1,41 @@
+// Coupons
+const coupons = [
+    "Food",
+    "Clothes",
+    "Toys",
+];
+const inputCoupon = document.getElementById("InputCoupon");
+const couponValue = inputCoupon.value;
+
+function onClickButtonPriceDiscount() {
+    const inputPrice = document.getElementById("InputPrice");
+    const priceValue = inputPrice.value;
+    
+    const inputCoupon = document.getElementById("InputCoupon");
+    const couponValue = inputCoupon.value;
+  
+    let discount;
+  
+    switch(couponValue) {
+      case "Food":
+        discount = 15;
+      break;
+      case "Clothes":
+        discount = 30;
+      break;
+      case "Toys":
+        discount = 25;
+      break;
+    }
+  
+  
+    const priceDiscount = calculateDiscounte(priceValue, discount);
+  
+    const resultP = document.getElementById("ResultP");
+    resultP.innerText = "El precio con descuento son: $" + priceDiscount;
+  }
+
+// Discontes
 function calculateDiscounte(){
     const input1 = document.getElementById("InputPrice");
     const precie = parseInt(input1.value);
