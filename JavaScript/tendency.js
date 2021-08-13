511f037ddf217bc095ce26afa09bee30cbf1127e
Camila Arancibia <camilachillan99@gmail.com>
2021-08-13 16:40:56 -0400

    The median function was created

--- JavaScript/tendency.js
+++ JavaScript/tendency.js
@@ -13,3 +13,13 @@ function calculateAverage(list){
     );
     return sumList/list.length;
 }
+function calculateMedian(list){
+    if(list.length%2 === 0){
+        const element1 = list[list.length/2];
+        const element2 = list[list.length/2 - 1];
+        return calculateAverage([element1,element2]);
+    }
+    else{
+        return list[parseInt(list.length/2)];
+    }
+}
