0a752e8125467395abd5127b149ac5ca39af8747
Camila Arancibia <camilachillan99@gmail.com>
2021-08-13 22:05:40 -0400

    The functions were implemented

--- JavaScript/tendency.js
+++ JavaScript/tendency.js
@@ -1,32 +1,52 @@
-const list1 = [
-    100,
-    300,
-    500,
-    200,
-    100
-   
-];
+var list = [];
 
-function calculateAverage(list){
+function add(){
+    const array = document.getElementById("textArray");
+    const number = document.getElementById("InputNumber");
+    const value = parseInt(number.value);
+    if(isNaN(value)){
+        array.innerText = list+"\nNo has ingresado ningún número";
+    }
+    else{
+        list.push(value);
+        array.innerText = list;
+    }
+}
+function eraser(){
+    const array = document.getElementById("textArray");
+    array.innerText = "Se ha eliminado la lista";
+    list = [];
+}
+
+function calculateAverage(){
+    const array = document.getElementById("textResult");
     const sumList = list.reduce(
         function(accumulatedValue = 0, newValue){
             return accumulatedValue + newValue;
         }
     );
-    return sumList/list.length;
+    if (array.innerText !== "La mediana es: "){
+        array.innerText = "El promedio es: " + sumList/list.length;
+    }else{
+        array.innerText += sumList/list.length;
+    }
+    
 }
-function calculateMedian(list){
+function calculateMedian(){
+    const array = document.getElementById("textResult");
     list.sort();
     if(list.length%2 === 0){
         const element1 = list[list.length/2];
         const element2 = list[list.length/2 - 1];
-        return calculateAverage([element1,element2]);
+        calculateAverage([element1,element2]);
+        array.innerText ="La mediana es: " + array.innerText.substr(15,array.innerText.length);
     }
     else{
-        return list[parseInt(list.length/2)];
+        array.innerText = "La mediana es: "+list[parseInt(list.length/2)];
     }
 }
-function calculateMode(list){
+function calculateMode(){
+    const array = document.getElementById("textResult");
     const listCount = {};
     list.map(
         function(element){
@@ -43,5 +63,5 @@ function calculateMode(list){
             return element2[1] - element1[1];
         }
     );
-    return listArray[0];
+    array.innerText = "La moda es: "+listArray[0];
 }
